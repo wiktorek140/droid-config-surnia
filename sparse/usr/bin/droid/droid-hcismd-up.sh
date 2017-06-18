9b3f72f7e13bda481adaf67201725d35c0ae6716
Wiktor <wiktorek140@gmail.com>
2017-06-19 01:34:29 +0200

    Update hcismd script

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -5,11 +5,19 @@
 # not work, so we loop.
 MAXTRIES=15
 
+#setprop bluetooth.hciattach true
+setprop ro.qualcomm.bt.hci_transport smd
+setprop qcom.bt.dev_power_class 2
+setprop qcom.bt.le_dev_pwr_class 2
+
 i=1
 while [ ! $i -gt $MAXTRIES ]  ; do
+    rfkill unblock all
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
-        # found hci0, get/set BT MAC address
+        rfkill unblock all
+        hciconfig hci0 up
+        # found hci0, exit successfully
         echo 0 > /sys/module/hci_smd/parameters/hcismd_set
         bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
         echo "BT MAC: $bt_mac"
@@ -25,5 +33,7 @@ while [ ! $i -gt $MAXTRIES ]  ; do
         # must have gotten through all our retries, fail
         exit 1
     fi
-
 done
+rfkill unblock all
+hciconfig hci0 up
+
